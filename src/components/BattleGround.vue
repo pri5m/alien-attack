fc174dee3adc32dbd3b8750656040517f6becf72
Holly Smith <ilex.laurel.smith@gmail.com>
2021-04-13 14:59:17 +0100

    Refactored out BaseHealthBar component

--- src/components/BattleGround.vue
+++ src/components/BattleGround.vue
@@ -1,44 +1,19 @@
 <template>
-  <div class="p-grid">
-    <div class="p-col-12">
-      <h2>Alien Health</h2>
-    </div>
-    <div class="p-col-12" id="healthbar">
-      <ProgressBar id="alienHealthBar" :value="alienHealth" />
-    </div>
-    <div class="p-col-12">
-      <h2>Your Health</h2>
-    </div>
-    <div class="p-col-12" id="healthbar">
-      <ProgressBar id="playerHealthBar" :value="playerHealth"></ProgressBar>
-    </div>
-  </div>
+  <BaseHealthBar title="Alien Health" :value="alienHealth" />
+  <section style="height: 60vh"></section>
+  <BaseHealthBar title="Your Health" :value="playerHealth" />
 </template>
 
 <script>
   import { mapGetters } from "vuex";
-  import ProgressBar from "primevue/progressbar";
+  import BaseHealthBar from "./ui/BaseHealthBar.vue";
   export default {
     name: "BattleGround",
     components: {
-      ProgressBar,
+      BaseHealthBar,
     },
     computed: {
       ...mapGetters(["alienHealth", "playerHealth"]),
     },
   };
 </script>
-
-<style scoped>
-  .p-col-12 {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  #healthbar {
-    display: block;
-  }
-  #playerHealthBar #monsterHealthBar {
-    width: 80%;
-  }
-</style>
---
+++ src/components/ui/BaseHealthBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="p-grid">
+    <div class="p-col-12">
+      <h2>{{ title }}</h2>
+    </div>
+    <div class="p-col-12" id="healthbar">
+      <ProgressBar id="healthBar" :value="value" />
+    </div>
+  </div>
+</template>
+
+<script>
+  import ProgressBar from "primevue/progressbar";
+  export default {
+    name: "BaseHealthBar",
+    components: {
+      ProgressBar,
+    },
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+      value: {
+        type: Number,
+        required: true,
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .p-col-12 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  #healthbar {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 90%;
+  }
+</style>
